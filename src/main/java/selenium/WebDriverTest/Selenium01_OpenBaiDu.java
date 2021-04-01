4a7cb7d4dad6923f7bb06a2ea0c9a6e466c29511
zipei0925 <425898860@qq.com>
2021-04-01 11:32:15 +0800

    Merge https://github.com/zipei0925/Selenium
    # Conflicts:
    #	src/main/java/selenium/WebDriverTest/Selenium01_OpenBaiDu.java
    #	src/main/java/selenium/WebDriverTest/Selenium10_SwitchToIframe.java
    #	target/classes/selenium/WebDriverTest/OpenBaiDu.class

---
+++ src/main/java/selenium/WebDriverTest/Selenium01_OpenBaiDu.java
@@ -0,0 +1,29 @@
+package selenium.WebDriverTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Selenium01_OpenBaiDu {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","D:\\Google\\Chrome\\Application\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.get("https://www.baidu.com");
+        try {
+            String baiDuTitle = "百度,知道";
+            assert baiDuTitle == driver.getTitle();
+            System.out.println("Test Pass");
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //关闭浏览器退出
+        driver.quit();
+    }
+}
